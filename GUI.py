8a25fdb848c16c6b0517e1b9d93e42b3933e46c9
Eugenio <lugezz@gmail.com>
2022-09-19 08:28:01 -0300

    GUI 1

--- GUI.py
+++ GUI.py
@@ -1,11 +1,11 @@
 from tkinter import *
 
-window = Tk() #instantiate an instance of a window
+window = Tk() # Instantiate an instance of a window
 window.geometry("420x420")
 window.title("Bro Code first GUI program")
 
-icon = PhotoImage(file='logo.png')
-window.iconphoto(True,icon)
-window.config(background="#5cfcff")
+icon = PhotoImage(file='data/logo2.png')
+window.iconphoto(True, icon)
+window.config(background = "#5cfcff")
 
-window.mainloop() #place window on computer screen, listen for event
+window.mainloop() # Place window on computer screen, listen for event
